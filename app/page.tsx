2c06a17eb4365752ba5cc91b21913e3998914869
Kengwong <Kengkeka@gmail.com>
2026-05-20 16:42:36 +0700

    fix: update google script url

--- app/page.tsx
+++ app/page.tsx
@@ -124,7 +124,7 @@ export default function Home() {
 
     try {
       alert("กำลังตรวจสอบรหัส VIP กรุณารอสักครู่นะคะ...");
-      const res = await fetch("https://script.google.com/macros/s/AKfycby_p5WC3SRfLOg1PGAS8zUQoVK9Q7oYnqRE0gqzyAxPLm6aLt1_R3Us_P_YFAS4mi50VQ/exec", {
+      const res = await fetch("https://script.google.com/macros/s/AKfycbx8hstNylnPcp0s-gDyOOBurS5DxFX1xFYPwY2hLyCSIUwG7dUGcgtiYzOC3aHZkUz5/exec", {
         method: "POST",
         body: JSON.stringify({ action: "verify", key: licenseKey })
       });
